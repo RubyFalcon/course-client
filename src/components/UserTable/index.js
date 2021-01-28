00476cbfe6b88855c0a296bc8f636117aeab1896
RubyFalcon <tupai@live.co.uk>
2021-01-28 17:00:08 +0000

    modified user Table

--- src/components/UserTable/index.js
+++ src/components/UserTable/index.js
@@ -1,13 +1,14 @@
-import { Container, Stack } from "@chakra-ui/react";
+import { Container, Heading, Stack } from "@chakra-ui/react";
 import { Table, Th, Tbody, Td, Tr, Thead } from "../Table/Table";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { useGlobalContext } from "../../Context/Context";
 /**
  * the UserTable is a Table to Display all the users who signed up, it uses useState and getUserData
  */
 const UserTable = () => {
-  const [userData, setUserData] = useState([]);
+  const { userData } = useGlobalContext();
+  const [allUserData, setAllUserData] = useState([]);
   /**
    * runs on render
    */
@@ -21,9 +22,8 @@ const UserTable = () => {
   const getUserData = async () => {
     try {
       const response = await axios.get("http://localhost:5000/user/");
-      console.log(response);
 
-      setUserData(response.data.data);
+      setAllUserData(response.data.data);
     } catch (error) {
       console.log(error);
     }
@@ -32,28 +32,32 @@ const UserTable = () => {
   return (
     <Stack as="student">
       <Container maxW="1280px">
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>First Name</Th>
-              <Th>Last Name</Th>
-              <Th>User Id</Th>
-              <Th>Address</Th>
-              <Th>Date Of Birth</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {userData.map((user) => (
+        {userData.First_Name ? (
+          <Table>
+            <Thead>
               <Tr>
-                <Td>{user.First_Name}</Td>
-                <Td>{user.Last_Name}</Td>
-                <Td>{user.ID}</Td>
-                <Td>{user.Address}</Td>
-                <Td>{user.DOB}</Td>
+                <Th>First Name</Th>
+                <Th>Last Name</Th>
+                <Th>User Id</Th>
+                <Th>Address</Th>
+                <Th>Date Of Birth</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {allUserData.map((user) => (
+                <Tr>
+                  <Td>{user.First_Name}</Td>
+                  <Td>{user.Last_Name}</Td>
+                  <Td>{user.ID}</Td>
+                  <Td>{user.Address}</Td>
+                  <Td>{user.DOB}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        ) : (
+          <Heading>You need to be signed in to see this</Heading>
+        )}
       </Container>
     </Stack>
   );
